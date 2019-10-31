33386a41f801d59bf5bfc9d82fe1c6b2fe64b5a0
Denis Tivikov <den.neposeda@gmail.com>
2019-10-31 16:03:21 +0400

    Task-03-unit-test: Generated tests are working well and pass ng test

--- src/app/components/courses-list/courses-list.component.spec.ts
+++ src/app/components/courses-list/courses-list.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesListComponent } from './courses-list.component';
+import { SearchComponent } from '../search/search.component';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { CourseComponent } from '../course/course.component';
+import { FormsModule } from '@angular/forms';
+import { DurationPipe } from 'src/app/pipe/duration.pipe';
 
 describe('CoursesListComponent', () => {
-  let component: CoursesListComponent;
-  let fixture: ComponentFixture<CoursesListComponent>;
+    let component: CoursesListComponent;
+    let fixture: ComponentFixture<CoursesListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CoursesListComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                CoursesListComponent,
+                SearchComponent,
+                FaIconComponent,
+                CourseComponent,
+                DurationPipe,
+            ],
+            imports: [
+                FormsModule,
+            ],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CoursesListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CoursesListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
