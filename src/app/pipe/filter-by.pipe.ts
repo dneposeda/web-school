3c7548cd2ef053fcb594efaf4b19a4c2895e5503
Andrey K <togliatti@gmail.com>
2019-11-11 11:20:45 +0400

    Review

--- src/app/pipe/filter-by.pipe.ts
+++ src/app/pipe/filter-by.pipe.ts
@@ -7,7 +7,7 @@ export class FilterByPipe implements PipeTransform {
 
     transform(items: any[], fieldName: string, search: string, ...args: any[]): any {
         if (!Array.isArray(items)) {
-            return;
+            return items;
         }
 
         if (fieldName) {
