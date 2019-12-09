f8cd10a9a0f1cc166427ac8ff928770591c60db2
Denis Tivikov <den.neposeda@gmail.com>
2019-12-09 12:42:40 +0400

    TAKS-06: Add component course-creare

---
+++ src/app/modules/courses/components/course-create/course-create.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-course-create',
+    templateUrl: './course-create.component.html',
+    styleUrls: ['./course-create.component.css']
+})
+export class CourseCreateComponent {
+
+    title;
+    description;
+    duration;
+    date;
+    authors;
+
+
+    constructor() { }
+
+    save(){
+
+    };
+
+    cancel(){
+
+    };
+
+}
--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CoursesRoutingModule } from './courses-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { PageComponent } from './components/page/page.component';
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
 import { CourseComponent } from './components/course/course.component';
 import { SearchComponent } from './components/search/search.component';
+import { CourseCreateComponent } from './components/course-create/course-create.component';
+
 import { HighlightDirective } from 'src/app/directives/highlight.directive';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -19,6 +21,7 @@ import { SharedModule } from '../shared/shared.module';
         CoursesListComponent,
         PageComponent,
         HighlightDirective,
+        CourseCreateComponent,
     ],
     imports: [
         FormsModule,
