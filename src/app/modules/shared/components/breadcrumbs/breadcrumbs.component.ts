06155053e2ea46172fe42366209bc105f2833699
Denis Tivikov <den.neposeda@gmail.com>
2019-12-19 01:16:58 +0400

    task-07: add breadcrubs component

--- src/app/modules/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/modules/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { IBreadcrumb } from 'src/app/interfaces/ibreadcrumb';
 
 @Component({
-  selector: 'app-breadcrumbs',
-  templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.css']
+    selector: 'app-breadcrumbs',
+    templateUrl: './breadcrumbs.component.html',
+    styleUrls: ['./breadcrumbs.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BreadcrumbsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class BreadcrumbsComponent {
+    @Input() items: IBreadcrumb[];
 
 }
