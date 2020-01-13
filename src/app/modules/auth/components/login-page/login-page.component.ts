10a2da37cbf736814f2785f8268a26ac62f7e7dc
Denis <den.neposeda@gmail.com>
2020-01-14 00:09:40 +0400

    [WIP] Task-11: Added template-driven form for login page

--- src/app/modules/auth/components/login-page/login-page.component.ts
+++ src/app/modules/auth/components/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import {Router} from '@angular/router';
 import { IUserAuth } from 'src/app/interfaces/iuser';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-login-page',
@@ -20,8 +21,14 @@ export class LoginPageComponent implements OnInit {
         };
     }
 
-    login() {
-        this.authService.login(this.user).subscribe(() => {
+
+    login(loginForm: NgForm): void {
+        // Form model
+        console.log(loginForm.form);
+        // Form value ( {"email":"mail@mail.ru","password":"123456"} )
+        console.log(`Saved: ${JSON.stringify(loginForm.value)}`);
+
+        this.authService.login(loginForm.value).subscribe(() => {
             this.router.navigate(['/']);
         });
     }
